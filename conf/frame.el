4b1af9ae0b35dc4f9584e227f07f194ea3fde882
2dice <rjhxx419@gmail.com>
2014-05-21 21:38:49 +0900

    Widen frame size

--- conf/frame.el
+++ conf/frame.el
@@ -1,7 +1,7 @@
 (setq initial-frame-alist
-      '( (width . 82) ))
+      '( (width . 83) ))
 (setq default-frame-alist
       (append (list 
-	'(width . 82)
+	'(width . 83)
 	)
 	default-frame-alist))
